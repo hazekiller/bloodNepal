a565741b0a4bd67e0ae23bbb726df4d797fb809f
hazekiller <prakashtimilsina76@gmail.com>
2025-01-05 12:33:28 +0545

    updated carousel

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -4,29 +4,40 @@ export default function Carousel() {
   return (
     <>
     <div className="flex justify-center items-center">
-    <div className="card bg-base-100 w-96 shadow-xl h-25">
+      <div className="card bg-base-100 w-96 shadow-xl h-25">
         <div className="card-body">
-        <h2 className="card-title">Shoes!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-  </div>
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-</div>
-<div className="card bg-base-100 w-96 shadow-xl">
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-  </div>
-  <figure>
-    <img
-      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
-      alt="Shoes" />
-  </figure>
-</div>
-</div>
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+        <figure>
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes" />
+        </figure>
+      </div>
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+        <figure>
+          <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes" />
+       </figure>
+      </div>
+      <div className="card bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title"> Prakah Timilina</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+        <figure>
+          <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes" />
+       </figure>
+      </div>
+    </div>
     </>
   )
 }
