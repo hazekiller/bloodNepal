e214540f87dfab8a9a89fffb919ebedcbaf5137e
hazekiller <prakashtimilsina76@gmail.com>
2025-01-05 17:39:56 +0545

    update carousel

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -3,8 +3,8 @@
 export default function Carousel() {
   return (
     <>
-    <div className="flex justify-center items-center">
-      <div className="card bg-base-100 w-96 shadow-xl h-25">
+    <div className="flex justify-center items-center mt-7 gap-7 ">
+      <div className="card bg-base-100 w-96 shadow-xl h-25 hover:cursor-pointer hover:scale-110 transition-all duration-300">
         <div className="card-body">
           <h2 className="card-title">Shoes!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -15,7 +15,7 @@ export default function Carousel() {
             alt="Shoes" />
         </figure>
       </div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+      <div className="card bg-base-100 w-96 shadow-xl hover:cursor-pointer hover:scale-110 transition-all duration-300">
         <div className="card-body">
           <h2 className="card-title">Shoes!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
@@ -26,7 +26,18 @@ export default function Carousel() {
           alt="Shoes" />
        </figure>
       </div>
-      <div className="card bg-base-100 w-96 shadow-xl">
+      <div className="card bg-base-100 w-96 shadow-xl hover:cursor-pointer hover:scale-110 transition-all duration-300">
+        <div className="card-body">
+          <h2 className="card-title"> Prakah Timilina</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+        </div>
+        <figure>
+          <img
+          src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+          alt="Shoes" />
+       </figure>
+      </div>
+      <div className="card bg-base-100 w-96 shadow-xl hover:cursor-pointer hover:scale-110 transition-all duration-300">
         <div className="card-body">
           <h2 className="card-title"> Prakah Timilina</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
