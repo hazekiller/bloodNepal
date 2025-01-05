1c5a5ddb61e95ec0420db9792c984f54e8d6d772
hazekiller <prakashtimilsina76@gmail.com>
2025-01-05 13:56:10 +0545

    update the App.jsx for routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,6 @@
+import Regiterform from "./components/Regiterform";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 
 function App() {
@@ -7,6 +9,8 @@ function App() {
   return (
     <>
       <Home/> 
+      <Login/>
+      <Regiterform/>
     </>
   )
 }
